969fc15221284a6aa016e2a7c4823cdfe0377110
expertman <javaexpert@nate.com>
2022-08-12 11:03:44 +0900

    Collections를 이용한 성적관리프로젝트

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("성적관리프로젝트 시작");
+		List<Student> list = new ArrayList<Student>(2);
+		Input input = new Input(list);
+		input.fileInput();
+			
+		Calc calc = new Calc(list);
+		calc.calc();
+		
+		Sort sort = new Sort(list);
+		sort.sort();
+		
+		Output output = new Output();
+		output.print(list);
+		System.out.println("성적관리프로젝트 끝");
+	}
+}
---
+++ src/Sort.java
@@ -0,0 +1,47 @@
+/*
+ * 객체 정렬하기
+ * 1. 배열 정렬하기
+ * 		-Arrays.sort()
+ * 		-Comparable interface를 재정의해야
+ * 		-compareTo()
+ * 2. Collection 정렬하기
+ * 		-Collections.sort()
+ * 		-Comparator interface를 재정의해야
+ * 		-compare()
+ */
+
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Sort {
+	private List<Student> list;
+
+	public Sort(List<Student> list) {
+		this.list = list;
+	}
+	
+	public void sort() {
+		Collections.sort(this.list, new Comparator<Student>() {
+			@Override
+			public int compare(Student front, Student back) {
+				//return back.getTot() - front.getTot();
+				return front.getName().compareTo(back.getName());
+			}
+		});
+	}
+	
+	//public void sort() {
+	//	Collections.sort(this.list, new MyComparator());
+	//}
+	
+//	class MyComparator implements Comparator<Student>{
+//
+//		@Override
+//		public int compare(Student front, Student back) {
+//			return back.getTot() - front.getTot();
+//		}
+//		
+//	}
+}
